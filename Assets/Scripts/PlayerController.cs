377b320a4446d6afa0ee0bdabbf62f3fdd2a7f2f
Khadijah Darragi <khadijahdarragi2003@gmail.com>
2024-10-06 20:42:27 +0100

    changed speed of player and attached main camera to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,6 @@ public class PlayerController : MonoBehaviour {
     void FixedUpdate () {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
 
-        GetComponent<Rigidbody>().AddForce(movement * speed * Time.
-            fixedDeltaTime);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
     }
 }
